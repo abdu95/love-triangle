8ca4473711242a958ff49677d72ba18a66a06be2
abdumalik2014 <abdumalik2014@gmail.com>
2019-07-06 23:59:36 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param preferences - an array of integers. Indices -|- Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
@@ -8,4 +8,9 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
         return 1;
     else if (preferences.length == 6)
         return 2;
+    for (var i = 0; i < length; i++) {
+
+    }
+    console.log("Array's " + (i + 1) + "th item, which is " + preferences[i] + " loves " + preferences[preferences[i] - 1]);
+
 };
